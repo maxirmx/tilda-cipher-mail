982c3b491794ccc48eb52ddf45224a67910186ab
maxirmx <maxirmx@.samsonov.net>
2023-03-05 12:36:32 +0300

    Tilda webhook (1)

--- html/tilda-webhook.php
+++ html/tilda-webhook.php
@@ -1,11 +1,25 @@
 <?php
+/**
+ * tilda-cipher-mail service
+ *
+ * Tilda callback
+ *
+ * @author  Максим Самсонов <maxim@samsonov.net>
+ * @copyright  2023 Максим Самсонов, его родственники и знакомые
+ * @license    http://www.opensource.org/license/mit-0 MIT
+ */
 
- header('Access-Control-Allow-Origin: *');
+ define("SERVICENAME", "tilda-cipher-mail");
+ define("DATAFILE",    "/var/".SERVICENAME."/data.txt");
+
+ header("Access-Control-Allow-Origin: *");
+
+ $data = print_r($_POST, true).PHP_EOL;
+ file_put_contents(DATAFILE, $data , FILE_APPEND | LOCK_EX) or die("Unable to append to file ".DATAFILE);
 
  $headers = "From: tilda-cipher-mail@samsonov.net";
- $message = print_r($_GET,true);
- @mail('maxim@samsonov.net', 'Tilda TEST', $message, $headers);
+
+ @mail('maxim@samsonov.net', 'Tilda TEST', $data, $headers);
 
  echo "ok";
-
 ?>
