a674084e6f25f5d849e5c0b3b6dd4dbca387adc3
maxirmx <maxirmx@.samsonov.net>
2023-03-09 20:03:18 +0300

    RC2

--- html/tilda-webhook.php
+++ html/tilda-webhook.php
@@ -16,12 +16,11 @@
 
  $_SERVER["REQUEST_METHOD"] === "POST" or die("POST method was expected, got ".$_SERVER["REQUEST_METHOD"]);
 
- $data = print_r(json_encode($_POST, JSON_FORCE_OBJECT), true).PHP_EOL;
+ $data = print_r(json_encode($_POST, JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE), true).PHP_EOL;
  file_put_contents(DATAFILE, $data , FILE_APPEND | LOCK_EX) or die("Unable to append to file ".DATAFILE);
 
- // [Send debug message when the data is received from tilda]
  // $headers = "From: tilda-cipher-mail <tilda-cipher-mail@1295435-cb87573.tw1.ru>";
- // @mail("your@email.net", "Tilda TEST", $data, $headers);
+ // @mail('maxim@samsonov.net', 'Tilda TEST', $data, $headers);
 
  echo "ok";
 ?>
