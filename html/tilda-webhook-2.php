2783b98ae0058a17e1d9bca0fb96265bba916488
maxirmx <maxirmx@.samsonov.net>
2023-03-23 12:43:16 +0300

    v0.2 RC2

---
+++ html/tilda-webhook-2.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * tilda-cipher-mail service
+ *
+ * Tilda callback 2
+ *
+ * @author  Максим Самсонов <maxim@samsonov.net>
+ * @copyright  2023 Максим Самсонов, его родственники и знакомые
+ * @license    http://www.opensource.org/license/mit-0 MIT
+ */
+
+ define("SERVICENAME", "tilda-cipher-mail-2");
+ define("DATAFILE",    "/var/".SERVICENAME."/data.txt");
+
+ header("Access-Control-Allow-Origin: *");
+
+ $_SERVER["REQUEST_METHOD"] === "POST" or die("POST method was expected, got ".$_SERVER["REQUEST_METHOD"]);
+
+ $data = print_r(json_encode($_POST, JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE), true).PHP_EOL;
+ file_put_contents(DATAFILE, $data , FILE_APPEND | LOCK_EX) or die("Unable to append to file ".DATAFILE);
+
+ echo "ok";
+?>
---
+++ opt/tilda-cipher-mail/job-2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# cron job
+#
+# Tilda daemon 2
+#
+# @author  Максим Самсонов <maxim@samsonov.net>
+# @copyright  2023 Максим Самсонов, его родственники и знакомые
+# @license    http://www.opensource.org/license/mit-0 MIT
+#
+
+set -o errexit -o pipefail -o noclobber -o nounset
+
+DIR0=$( dirname "$0" )
+SERVICENAME="tilda-cipher-mail-2"
+DATAFILE="/var/$SERVICENAME/data.txt"
+TMPFILE="/var/$SERVICENAME/tmp.txt"
+ENCFILE="/var/$SERVICENAME/data.enc.txt"
+JSONFILE="/var/$SERVICENAME/data.json"
+ENCJSONFILE="/var/$SERVICENAME/data.enc.json"
+CSVFILE="/var/$SERVICENAME/data.csv"
+ENCCSVFILE="/var/$SERVICENAME/data.enc.csv"
+CERTFILE="/etc/$SERVICENAME/certificate_1801993.cer"
+MSGFILE="/etc/$SERVICENAME/$SERVICENAME.msg"
+LOGFILE="/var/$SERVICENAME/log.txt"
+#LOGFILE="/dev/nul"
+CRYPTCP=/opt/cprocsp/bin/amd64/cryptcp
+EMAIL_FROM="sender-service <sender-service@${HOSTNAME}>"
+EMAIL_TO="your@mail"
+
+echo -n "$SERVICENAME daemon started on ">>"$LOGFILE"
+date>>"$LOGFILE"
+
+rm -f "$TMPFILE"
+rm -f "$JSONFILE"
+rm -f "$CSVFILE"
+
+if [[ -f "$DATAFILE" ]]; then
+  flock -w 5 -x "$DATAFILE" -c "mv $DATAFILE $TMPFILE"
+  sed '1s/^/[/;$!s/$/,/;$s/$/]/'  "$TMPFILE" > "$JSONFILE"
+  "$DIR0"/convert.php "$JSONFILE" "$CSVFILE"
+else
+  echo "Нет данных" > "$TMPFILE"
+  echo "[ ]" > "$JSONFILE"
+  echo " " > "$CSVFILE"
+fi
+
+rm -f "$ENCFILE"
+rm -f "$ENCJSONFILE"
+rm -f "$ENCCSVFILE"
+"$CRYPTCP" -encr -f "$CERTFILE" "$TMPFILE" "$ENCFILE">>"$LOGFILE"
+"$CRYPTCP" -encr -f "$CERTFILE" "$JSONFILE" "$ENCJSONFILE">>"$LOGFILE"
+"$CRYPTCP" -encr -f "$CERTFILE" "$CSVFILE" "$ENCCSVFILE">>"$LOGFILE"
+
+EMAIL="$SERVICENAME <${SERVICENAME}@1295435-cb87573.tw1.ru>"
+mutt -e "my_hdr From:$EMAIL_FROM" -y -s "Данные от Tilda" -a "$ENCFILE" "$ENCJSONFILE" "$ENCCSVFILE" -- "$EMAIL_TO"<"$MSGFILE"
+
+echo -n "$SERVICENAME daemon finished on ">>"$LOGFILE"
+date>>"$LOGFILE"
